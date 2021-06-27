e6586a38348b9d9ae02e16b66b8135dc6163776f
Thisara Hettikankanam <43540833+Thisarahetz@users.noreply.github.com>
2021-06-27 11:11:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Info from "./Info";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
